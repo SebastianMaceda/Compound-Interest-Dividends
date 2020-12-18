436a7f9e99e750fb8e5df3eccdc4e6ca5d0a3931
SebastianMaceda <macedastm@gmail.com>
2020-12-18 17:41:00 -0600

    adding monthly contribution input

--- compoundCalculator.js
+++ compoundCalculator.js
@@ -1,19 +1,25 @@
 class calculator{
-    constructor(intialInvestment, lengthOfTime, interestRate){
-        this.intialInvestment = intialInvestment;
+    constructor(intialInvestment, lengthOfTime, interestRate, monthlyContribution){
+        this.intialInvestment = parseInt(intialInvestment);
         this.lengthOfTime = lengthOfTime;
         this.interestRate = interestRate/ 100;
+        this.monthlyContribution = monthlyContribution * 12;
     }
     get compoundInterestCalculator(){
         return this.compoundInfo();
     }
     compoundInfo() {
         const interestUl = document.querySelector('.interestPerYear');
-        let total = 0;
-        for (let i = 0; i < this.lengthOfTime; i++) {
+        let total = parseInt(intialCapital);
+        for (let i = 0; i <= this.lengthOfTime; i++) {
+            if (i > 0){
+                console.log(this.monthlyContribution);
+                this.intialInvestment += this.monthlyContribution;
+                console.log(i, this.intialInvestment);
+            }
+            
             this.intialInvestment += this.intialInvestment * this.interestRate;
             total = this.intialInvestment;
-
 
             const randomLi = document.createElement('li');
             randomLi.classList = "randomLi";
@@ -21,15 +27,46 @@ class calculator{
 
             randomLi.textContent = `${total}`
 
-            
-            console.log(total);
-            console.log(i);
             interestUl.appendChild(randomLi);
+            if (i == (this.lengthOfTime - 1)){
+                console.log(i);
+            }
+            
 
+            console.log(i, this.intialInvestment);
 
         }
         return this.intialInvestment;
     }
 }
 
-export {calculator}
+
+
+let intialCapital;
+let timeInYears;
+let interestRate;
+let monthlyContribution;
+
+// console.log(intialCapital, timeInYears, interestRate);
+
+const intialInvestmentInput = document.querySelector('#intialInvestmentInput');
+const lengthOfTimeInput = document.querySelector('#lengthOfTimeInput');
+const interestRateInput = document.querySelector('#interestRateInput');
+const monthlyContributionsInput = document.querySelector('#monthlyContributionsInput');
+
+function cheerio(event) {
+    event.preventDefault();
+
+    intialCapital = intialInvestmentInput.value;
+    timeInYears = lengthOfTimeInput.value;
+    interestRate = interestRateInput.value;
+    monthlyContribution = monthlyContributionsInput.value;
+
+    let p = new calculator(intialCapital, timeInYears, interestRate, monthlyContribution)
+
+    p.compoundInterestCalculator;
+    // console.log(intialCapital, timeInYears, interestRate);
+}
+
+
+export {calculator, cheerio}
--- main.js
+++ main.js
@@ -1,30 +1,10 @@
-import {calculator} from './compoundCalculator.js'
-
-let intialCapital;
-let timeInYears;
-let interestRate;
-
-const intialInvestmentInput = document.querySelector('#intialInvestmentInput');
-const lengthOfTimeInput = document.querySelector('#lengthOfTimeInput');
-const interestRateInput = document.querySelector('#interestRateInput');
-
-const calculateBtn = document.querySelector('#calculateBtn');
-
-function myFunction(e) {
-    console.log("bruv");
-    preventDefault();
-    
-    intialCapital = intialInvestmentInput.value;
-    timeInYears = lengthOfTimeInput.value;
-    interestRate = interestRateInput.value;
-
-    console.log(intialCapital, timeInYears, interestRate);
-}
-
-calculateBtn.addEventListener('submit', myFunction());
+import {calculator, cheerio} from './compoundCalculator.js';
 
 
 
-let p = new calculator(intialCapital, timeInYears, interestRate)
 
-p.compoundInterestCalculator;
+let calculateBtn = document.querySelector('#calculateBtn').addEventListener('click', cheerio);
+
+
+
+
